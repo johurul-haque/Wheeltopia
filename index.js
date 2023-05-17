1a4ad86e0c27a7b911111a8a71421819f3ac9430
Johurul Haque <johurulhaquejony@gmail.com>
2023-05-17 23:38:27 +0600

    connected to mongodb atlas

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors())
-app.get('/', (req, res) => res.send('<h1>Hello world!</h1>'))
+app.use(express.json())
+
+// const uri = 'mongodb://localhost:27017'
+
+const uri = "mongodb+srv://johurul-haque:PtPhbNwY7c4RFINt@learningdb.g9rdpy8.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+}); 
+
+async function run() {
+  try {
+    await client.connect()
+
+    app.get('/', (req, res) => res.send('<h1>Hello world!</h1>'))
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    )
+  } finally {
+    // await client.close()
+  }
+}
+run().catch(console.dir)
 
 app.listen(port, () => console.log(`app listening on port: ${port}`))
