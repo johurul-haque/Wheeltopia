0b0e3a6c714ca4a5855b42bc4a113fe72bc39b6c
Johurul Haque <johurulhaquejony@gmail.com>
2023-05-17 18:25:48 +0600

    renamed Private route

---
+++ src/routes/Private.jsx
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import Loading from "../components/Loading";
+import { AuthContext } from "../providers/AuthProvider";
+
+// eslint-disable-next-line react/prop-types
+const Private = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  let location = useLocation();
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (user) {
+    return children;
+  }
+
+  return <Navigate to={"/login"} state={{ from: location }} replace />;
+};
+export default Private;
