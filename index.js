310e6702e52b7045dbf180803d49072f587488a4
Johurul Haque <johurulhaquejony@gmail.com>
2023-05-20 18:26:43 +0600

    getting data from database and sending via get method

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
+const app = express();
+const port = process.env.PORT || 5000;
 
-const app = express()
-const port = process.env.PORT || 5000
-
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // const uri = 'mongodb://localhost:27017'
 
@@ -20,22 +19,28 @@ const client = new MongoClient(uri, {
     strict: true,
     deprecationErrors: true,
   },
-}); 
+});
 
 async function run() {
   try {
-    client.connect()
+    client.connect();
 
-    app.get('/', (req, res) => res.send('<h1>Hello world!</h1>'))
+    const database = client.db("toysDB").collection("toy");
+
+    app.get("/toys", async (req, res) => {
+      const cursor = database.find(),
+        result = await cursor.toArray();
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
-    )
+    );
   } finally {
     // await client.close()
   }
 }
-run().catch(console.dir)
+run().catch(console.dir);
 
-app.listen(port, () => console.log(`app listening on port: ${port}`))
+app.listen(port, () => console.log(`app listening on port: ${port}`));
