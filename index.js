186132c1680dbd587a53f47fe1cac816c3f04ec0
Johurul Haque <johurulhaquejony@gmail.com>
2023-06-16 16:56:54 +0600

    hidden mongodb uri

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const uri = 'mongodb://localhost:27017'
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@learningdb.g9rdpy8.mongodb.net/?retryWrites=true&w=majority`;
+const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri, {
   serverApi: {
